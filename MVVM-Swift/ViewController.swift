4c2eadeaef956fc3fcd83391aa68dab7df4f5162
Humberto Vieira <humbertovieira.castro@gmail.com>
2017-01-10 19:04:02 -0200

    Initial Commit

---
+++ MVVM-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVVM-Swift
+//
+//  Created by Humberto Vieira on 10/01/17.
+//  Copyright © 2017 Humberto Vieira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
